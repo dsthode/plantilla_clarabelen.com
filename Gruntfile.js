b0362f9ff722f5891045567fa46fb09efbeba542
Damian Serrano Thode <dsthode@gmail.com>
2014-01-02 23:42:29 +0100

    Added new tasks and changed file locations

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,19 +5,48 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				sourceMap: 'build/source.map.js',
+				report: 'min'
       },
       build: {
-        src: 'src/*.js',
+        src: 'src/js/*.js',
         dest: 'build/script.min.js'
       }
-    }
+    },
+		cssmin: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				report: 'min'
+			},
+			build: {
+				src: 'src/css/*.css',
+				dest: 'build/style.min.css'
+			}
+		},
+		copy: {
+			php: {
+				expand: true,
+				cwd: 'src',
+				src: '*.php',
+				dest: 'build/',
+				filter: 'isFile'
+			},
+			images: {
+				expand: true,
+				cwd: 'src',
+				src: 'images/*',
+				dest: 'build/',
+				filter: 'isFile'
+			}
+		}
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'cssmin', 'copy']);
 
 };
