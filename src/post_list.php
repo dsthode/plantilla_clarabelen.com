108b2b6cd9712acf25e984e5cbd380540a30738c
Damian Serrano Thode <dsthode@gmail.com>
2014-01-09 22:49:14 +0100

    added box for image in post list

--- src/post_list.php
+++ src/post_list.php
@@ -1,6 +1,8 @@
 <?php while(have_posts()) : the_post(); ?>
 	<div class="post media">
-		<a class="pull-left" href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium', array('class' => 'media-object')); ?></a>
+		<div class="post-featured-image">
+			<a class="pull-left" href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium', array('class' => 'media-object')); ?></a>
+		</div>
 		<div class="media-body">
 			<p><?php the_date(); ?> | <?php comments_number('Sin comentarios', 'Un comentario', '% comentarios'); ?> | por <?php the_author(); ?></p>
 			<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
