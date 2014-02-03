63b568bafc92943de957fcdb812085c243e17904
Damian Serrano Thode <dsthode@gmail.com>
2014-02-03 20:59:14 +0100

    fixes

--- src/js/submenu.js
+++ src/js/submenu.js
@@ -1,10 +1,10 @@
-$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+jQuery('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
         // Avoid following the href location when clicking
         event.preventDefault(); 
         // Avoid having the menu to close when clicking
         event.stopPropagation(); 
         // If a menu is already open we close it
-        $('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
+        jQuery('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
         // opening the one you clicked on
-        $(this).parent().addClass('open');
-    });
+        jQuery(this).parent().addClass('open');
+});
