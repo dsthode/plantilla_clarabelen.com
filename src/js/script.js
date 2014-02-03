8f21c39a326753f367867b8b5ceb6f28f06a16d4
Damian Serrano Thode <dsthode@gmail.com>
2014-02-04 00:59:43 +0100

    changed thumbnail size

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,6 @@ var sc_project=5906083;
 var sc_invisible=1; 
 var sc_security="a1822d2d";
 
-jQuery('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
-        // Avoid following the href location when clicking
-        event.preventDefault(); 
-        // Avoid having the menu to close when clicking
-        event.stopPropagation(); 
-        // If a menu is already open we close it
-        jQuery('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
-        // opening the one you clicked on
-        jQuery(this).parent().addClass('open');
-});
-
 try {
 	(adsbygoogle = window.adsbygoogle || []).push({});
 	(adsbygoogle = window.adsbygoogle || []).push({});
@@ -50,10 +39,23 @@ function loadAdsense() {
 	s.parentNode.insertBefore(adsrc, s);
 }
 
+function initializeSubmenus() {
+	jQuery('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+		// Avoid following the href location when clicking
+		event.preventDefault(); 
+		// Avoid having the menu to close when clicking
+		event.stopPropagation(); 
+		// If a menu is already open we close it
+		jQuery('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
+		// opening the one you clicked on
+		jQuery(this).parent().addClass('open');
+	});
+}
 function loadThingsOnLoad(){
 	loadTranslator();
 	loadStatCounter();
 	loadAdsense();
+	initializeSubmenus();
 }
 
 if (window.addEventListener) {
