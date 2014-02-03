e4d38e72f00f64291fc16db1fabee61b80207bf8
Damian Serrano Thode <dsthode@gmail.com>
2014-02-03 21:28:46 +0100

    fixes

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,17 @@ var sc_project=5906083;
 var sc_invisible=1; 
 var sc_security="a1822d2d";
 
+jQuery('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+        // Avoid following the href location when clicking
+        event.preventDefault(); 
+        // Avoid having the menu to close when clicking
+        event.stopPropagation(); 
+        // If a menu is already open we close it
+        jQuery('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
+        // opening the one you clicked on
+        jQuery(this).parent().addClass('open');
+});
+
 try {
 	(adsbygoogle = window.adsbygoogle || []).push({});
 	(adsbygoogle = window.adsbygoogle || []).push({});
